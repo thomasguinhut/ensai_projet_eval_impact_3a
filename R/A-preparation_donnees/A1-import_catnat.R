0e226617e41941f2ea3b7d266e284aadd10320ab
thomasguinhut <thomas.guinhut.24@gmail.com>
2025-12-02 21:40:03 +0000

    maj catnat

--- R/A-preparation_donnees/A1-import_catnat.R
+++ R/A-preparation_donnees/A1-import_catnat.R
@@ -1,4 +1,4 @@
-catnat <-
+catnat_1 <-
   aws.s3::s3read_using(
     FUN = read.csv2,
     object = "projet_eval_impact/catnat_gaspar.csv",
@@ -6,4 +6,46 @@ catnat <-
     opts = list("region" = "")
   )
 
-glimpse(catnat)
+glimpse(catnat_1)
+
+catnat_2 <- catnat_1 %>%
+  dplyr::select(-c("dat_pub_arrete", "dat_pub_jo", "dat_maj"))
+
+# Il apparaît que dans les deux variables dates retenues, il y a deux
+# formatages : AAAA-MM-JJ et AAAA-MM-JJ HH-MM-SS. On vérifie qu'il n'y a pas
+# d'autres formatages.
+est_format_date <- Vectorize(function(x) {
+  !is.na(x) && (
+    str_detect(x, "^\\d{4}-\\d{2}-\\d{2}$") ||
+      str_detect(x, "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}$")
+  )
+})
+
+catnat_2 %>%
+  filter(!est_format_date(dat_deb)) %>%
+  distinct(dat_deb) %>%
+  pull(dat_deb)
+
+catnat_2 %>%
+  filter(!est_format_date(dat_fin)) %>%
+  distinct(dat_fin) %>%
+  pull(dat_fin)
+
+# Pas d'autres formats, on convertie en format Date en prenant les premiers
+# XXXX-XX-XX
+catnat_3 <- catnat_2 %>% 
+  mutate(
+    dat_deb = as.Date(substr(dat_deb, 1, 10)), 
+    dat_fin = as.Date(substr(dat_fin, 1, 10))
+  )
+
+# On vérifie qu'il n'y a pas de modalité étrange
+unique(catnat_3$lib_risque_jo)
+
+summary(catnat_3)
+
+# On vérifie qu'il n'y a pas de NA
+catnat_3 %>%
+  summarise(across(everything(), ~ sum(is.na(.x))))
+
+catnat <- catnat_2
